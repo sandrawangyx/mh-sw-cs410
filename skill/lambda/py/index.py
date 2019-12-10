97c537576f18e931f0c89e66d734a5c07a8f7bf1
Miao Huang <miaohuang910917@gmail.com>
2019-12-09 22:16:18 -0500

    add basic alexa skill

--- skill/lambda/py/index.py
+++ skill/lambda/py/index.py
@@ -5,15 +5,15 @@
 # session persistence, api calls, and more.
 # This sample is built using the handler classes approach in skill builder.
 import logging
-
-from ask_sdk_core.skill_builder import SkillBuilder
-from ask_sdk_core.dispatch_components import AbstractRequestHandler
-from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.dispatch_components import (
+    AbstractRequestHandler, AbstractExceptionHandler,
+    AbstractRequestInterceptor, AbstractResponseInterceptor)
 import ask_sdk_core.utils as ask_utils
 from ask_sdk_core.handler_input import HandlerInput
 
 from ask_sdk_model import Response
-from ./movies import popular_movies
+from ask_sdk.standard import StandardSkillBuilder
+from alexa import data
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -27,12 +27,13 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Welcome, you can say Hello or Help. Which would you like to try?"
+        speak_output = "Welcome to Smart Movies!"
 
         return (
             handler_input.response_builder
                 .speak(speak_output)
                 .ask(speak_output)
+                .set_should_end_session(True)
                 .response
         )
 
@@ -45,10 +46,8 @@ class PopularMoviesIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        popular_movies = popular_movies()
-        console.log(popular_movies)
         speak_output = "Here are a few popular movies you might like: {}, {}, {}"
-        speech = speak_output.format(popular_movies[0].movie_title, popular_movies[1].movie_title,popular_movies[2].movie_title)
+        # speech = speak_output.format(popular_movies[0].movie_title, popular_movies[1].movie_title,popular_movies[2].movie_title)
 
 
         return (
@@ -153,11 +152,67 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
                 .response
         )
 
-# The SkillBuilder object acts as the entry point for your skill, routing all request and response
-# payloads to the handlers above. Make sure any new handlers or interceptors you've
-# defined are included below. The order matters - they're processed top to bottom.
+# ###################################################################
 
-sb = SkillBuilder()
+# ############# REQUEST / RESPONSE INTERCEPTORS #####################
+class RequestLogger(AbstractRequestInterceptor):
+    """Log the alexa requests."""
+    def process(self, handler_input):
+        # type: (HandlerInput) -> None
+        logger.debug("Alexa Request: {}".format(
+            handler_input.request_envelope.request))
+
+
+class LoadPersistenceAttributesRequestInterceptor(AbstractRequestInterceptor):
+    """Check if user is invoking skill for first time and initialize preset."""
+    def process(self, handler_input):
+        # type: (HandlerInput) -> None
+        persistence_attr = handler_input.attributes_manager.persistent_attributes
+
+        if len(persistence_attr) == 0:
+            # First time skill user
+            persistence_attr["playback_setting"] = {
+                "loop": False,
+                "shuffle": False
+            }
+
+            persistence_attr["playback_info"] = {
+                "play_order": [l for l in range(0, len(data.AUDIO_DATA))],
+                "index": 0,
+                "offset_in_ms": 0,
+                "playback_index_changed": False,
+                "token": None,
+                "next_stream_enqueued": False,
+                "in_playback_session": False,
+                "has_previous_playback_session": False
+            }
+        else:
+            # Convert decimals to integers, because of AWS SDK DynamoDB issue
+            # https://github.com/boto/boto3/issues/369
+            playback_info = persistence_attr.get("playback_info")
+            playback_info["index"] = int(playback_info.get("index"))
+            playback_info["offset_in_ms"] = int(playback_info.get(
+                "offset_in_ms"))
+            playback_info["play_order"] = [
+                int(l) for l in playback_info.get("play_order")]
+
+
+class ResponseLogger(AbstractResponseInterceptor):
+    """Log the alexa responses."""
+    def process(self, handler_input, response):
+        # type: (HandlerInput, Response) -> None
+        logger.debug("Alexa Response: {}".format(response))
+
+
+class SavePersistenceAttributesResponseInterceptor(AbstractResponseInterceptor):
+    """Save persistence attributes before sending response to user."""
+    def process(self, handler_input, response):
+        # type: (HandlerInput, Response) -> None
+        handler_input.attributes_manager.save_persistent_attributes()
+# ###################################################################
+
+sb = StandardSkillBuilder(
+    table_name=data.DYNAMODB_TABLE_NAME, auto_create_table=True)
 
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(PopularMoviesIntentHandler())
@@ -167,5 +222,14 @@ sb.add_request_handler(SessionEndedRequestHandler())
 sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
 
 sb.add_exception_handler(CatchAllExceptionHandler())
+
+
+# Interceptors
+sb.add_global_request_interceptor(RequestLogger())
+sb.add_global_request_interceptor(LoadPersistenceAttributesRequestInterceptor())
+
+sb.add_global_response_interceptor(ResponseLogger())
+sb.add_global_response_interceptor(SavePersistenceAttributesResponseInterceptor())
+
 
 handler = sb.lambda_handler()
